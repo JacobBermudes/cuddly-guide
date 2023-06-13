da0019aefe6e7d7674a7262cdbb4677cf2c80ff4
Jacob Bermudes <marriagesebers@gmail.com>
2023-06-13 21:13:18 +0300

    auth page added

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
+const PORT = 120
+const getPage = (pageName) => path.resolve(__dirname, 'pages', `${pageName}.html`)
+const getEjsPage = (pageName) => path.resolve(__dirname, 'pages', 'ejsPages',`${pageName}.ejs`)
 
-app.get('/test', (req, res)=>{
-    res.send('Hello world')
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res)=>{
+    res.render(getEjsPage('index'))
 })
 
-const PORT = 120
+app.get('/auth', (req, res)=>{
+    res.render(getEjsPage('auth'))
+})
+
+app.get('/oldIndex', (req, res)=>{
+    res.sendFile(getPage('index'))
+})
+
+app.get('/error', (req, res)=>{
+    res.sendFile(getPage('error'))
+    console.log('SomeoneGetError')
+})
+
 app.listen(PORT, ()=>{
     console.log(`Server was started on localhost:${PORT}`)
 }) 
